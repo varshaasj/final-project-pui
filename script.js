6f6d101314b5ac5984dbb1e81fda6304f4f529da
Varshaa <vsekarja@andrew.cmu.edu>
2023-05-02 11:24:34 -0400

    new changes made

---
+++ script.js
@@ -0,0 +1,8 @@
+function openPopup() {
+    document.getElementById('popup').classList.add('show-popup');
+  }
+  
+  function closePopup() {
+    document.getElementById('popup').classList.remove('show-popup');
+  }
+
